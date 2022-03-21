b081d504185654b92cdec254db41af7bfc8cd8bf
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-21 19:14:40 +0530

    Added BrowerRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
     <div className="App">
     <Header/>
     <Routes>
@@ -14,7 +13,6 @@ function App() {
     </Routes>
     <Footer/>
     </div>
-    </Router>
   );
 }
 export default App;
