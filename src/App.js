728f576ba51a618449f945a0caedb6050611de65
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-23 18:02:20 +0530

    Added filtercontext and productlist route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Productlist from "./pages/Productlist";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/productlist/:type" element={<Productlist/>}/>
     </Routes>
     <Footer/>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router} from 'react-router-dom' 
 import { makeServer } from "./server";
+import {FilterContextProvider} from './context/filter-context'
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <FilterContextProvider>
       <App />
+    </FilterContextProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
