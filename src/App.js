c4507c2e78c1e98958fcdd60cd41cb311c842099
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-25 14:56:26 +0530

    made a separate file for component imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Footer from "./components/Footer";
-import Productlist from "./pages/Productlist";
+import {Home, Header, Footer, Productlist} from "./components/import.js"
 
 function App() {
   return (
